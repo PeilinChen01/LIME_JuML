cf356158003bd894fe51dd3018616ca44a9ab225
LongKhanhDuong <LongKhanh_Duong@hotmail.de>
2024-07-07 10:25:56 +0200

    Update superpixel_lime.jl
    add average_color function:
    
    function calculates the average color of each superpixel
    
    add perturb_image function:
    
    this function deactives random superpixels
    
    add plot_label
    
    this function plots the given label number

--- src/superpixel_lime.jl
+++ src/superpixel_lime.jl
@@ -3,47 +3,103 @@ using TestImages
 using ImageSegmentation
 using ColorTypes
 using Plots
-using Random  # Importiere das Random-Paket für die zufällige Farbgenerierung
-using DataStructures  # Importiere das DataStructures-Paket für die Frequenzzählung
 
-
-# Lade ein Beispielbild
+# Load image
 img = load("Image/Clownfish.jpg")
 # img = Gray.(testimage("house"))
 
-# Führe die Felzenszwalb-Superpixel-Segmentierung durch
-segments = felzenszwalb(img, 10, 100)  # 10 ist der Schwellwert-Wert, 50 mindestgröße der Superpixel
+height, width = size(img)
 
-# Funktion zum Erzeugen einer zufälligen Farbe basierend auf einem Seed-Wert
-function get_random_color(seed)
-    Random.seed!(seed)  # 
-    rand(RGB{N0f8})  # Generiere eine zufällige Farbe
+# Felzenszwalb superpixel segmentation
+segments = felzenszwalb(img, 1, 1)  # Input: image, scale parameter, minimum size of superpixels
+
+# Generate superpixel labels
+superpixel_labels = labels_map(segments)
+
+# Function to generate the average color of the superpixels
+function average_color(img, labels, label)
+    if eltype(img) <: Gray
+        total = 0.0
+        count = 0
+        for i in 1:size(img, 1)
+            for j in 1:size(img, 2)
+                if labels[i, j] == label
+                    total += Float64(img[i, j])
+                    count += 1
+                end
+            end
+        end
+        gray_value = total / count
+        return RGB(gray_value, gray_value, gray_value)
+    else
+        r_total = 0.0
+        g_total = 0.0
+        b_total = 0.0
+        count = 0
+        for i in 1:size(img, 1)
+            for j in 1:size(img, 2)
+                if labels[i, j] == label
+                    color = img[i, j]
+                    r_total += red(color)
+                    g_total += green(color)
+                    b_total += blue(color)
+                    count += 1
+                end
+            end
+        end
+        return RGB(r_total / count, g_total / count, b_total / count)
+    end
 end
 
-# Erzeuge eine Farbbild-Darstellung der Superpixel-Segmente
-# Erstelle eine Zufallskarte für die Superpixel-Labels
-superpixel_labels = labels_map(segments)  # Extrahiere die Labels der Superpixel
-colors = [get_random_color(i) for i in 0:maximum(superpixel_labels)]  # Generiere zufällige Farben für jedes Label
-seg_colored = [colors[label+1] for label in superpixel_labels]  # Wende die Farben auf die Superpixel an
+# Number of superpixels
+max_label = maximum(superpixel_labels)
 
-# Visualisierung der Superpixel-Segmente
-seg_img = reshape(seg_colored, size(img))  # Reshape der Segmentierung zu einem Bild
+# Generating the average color of each superpixel
+avg_colors = [average_color(img, superpixel_labels, label) for label in 0:max_label]
+seg_colored = [avg_colors[label+1] for label in superpixel_labels]
+
+# Plot the superpixels
+seg_img = reshape(seg_colored, size(img))
 plot(heatmap(seg_img), title="Felzenszwalb Superpixel Segmentation", size=(800, 600))
 
+# Function to perturb the image with deactivated superpixels
+function perturb_image(img, superpixels, deactived=100)
+    num_superpixels = maximum(superpixels)
+    deactived_superpixels = rand(Bool, deactived, num_superpixels)
+    perturbed_images = []
+    
+    for sample in eachrow(deactived_superpixels)
+        perturbed_img = copy(img)
+        for i in 1:num_superpixels
+            if !sample[i]
+                perturbed_img[superpixels .== i] .= 0.0
+            end
+        end
+        push!(perturbed_images, perturbed_img)
+    end
+    
+    return perturbed_images, deactived_superpixels
+end
+
+# Generate perturbed images
+perturbed_images, deactived_superpixels = perturb_image(seg_img, superpixel_labels)
+
+# Plot one of the perturbed images
+plot(heatmap(perturbed_images[100]), title="Perturbed Felzenszwalb Superpixel Segmentation", size=(800, 600))
+
+# Function to isolate and plot a superpixel in the original image
 function plot_label(label, superpixel_labels, img)
-    # Erstelle eine Maske für das angegebene Label
+    # Create mask for the chosen superpixel
     mask = superpixel_labels .== label
 
-    # Erzeuge ein Farbbild, das nur das angegebene Label hervorhebt
+    # Set all other pixels to black
     highlighted_img = copy(img)
-    highlighted_img[.!(mask)] .= RGB(0.0, 0.0, 0.0)  # Setze alle Pixel, die nicht zum Label gehören, auf schwarz
+    highlighted_img[.!(mask)] .= RGB(0.0, 0.0, 0.0)
 
-    # Plotte das Ergebnis
+    # Plot the result
     plot(highlighted_img, title="Superpixel Label $label", size=(800, 600))
 end
 
-
-# Wähle ein Label zum Plotten (zum Beispiel das erste Label)
-label_to_plot = 2  # Beispiel: 0 ist das erste Label
+# Example to plot a specific superpixel
+label_to_plot = 2
 plot_label(label_to_plot, superpixel_labels, img)
-
