8f6ee9a6dc472015e5085e817b2bfd4668fd7eae
LongKhanhDuong <LongKhanh_Duong@hotmail.de>
2024-07-14 14:47:13 +0200

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,16 @@
 using LIME_JuML
 using Test
-using XAIBase
 
 @testset "LIME_JuML.jl" begin
     # Write your tests here.
-    @testset "sample around test" begin
-        @test sample_around([1; 1], 1) == [[0.878584298512076, 1.0872814156171304]]
-    end
-    @testset "similarity kernel test" begin
-        @test similarity_kernel([1.0, 1.0], [1.1, 1.1], 1.0) == 0.9801986733067553
-    end
-    @testset "sparse linear explanations test" begin
-        @test sparse_linear_explanations(x -> sum(x), [1.0, 1.0], [1.1, 1.1], 10, 1)[1] == 0.9706216694190505
+    @testset "superpixel test" begin
+        
+        img = load("Image/test.jpg")
+        
+        segments = felzenszwalb(img, 1, 20)
+        superpixel_labels = labels_map(segments)
+        max_label = maximum(superpixel_labels)
+
+        @test perturbed_images, deactivated_superpixels = perturb_image(img, superpixel_labels, 2)
     end
 end
