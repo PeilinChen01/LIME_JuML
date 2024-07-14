04ff2ad51e2cb85372f5b98f79081b6813cdccd8
LongKhanhDuong <LongKhanh_Duong@hotmail.de>
2024-07-14 14:26:31 +0200

    Bug Fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,15 +5,12 @@ using XAIBase
 @testset "LIME_JuML.jl" begin
     # Write your tests here.
     @testset "sample around test" begin
-        Random.seed!(111)
         @test sample_around([1; 1], 1) == [[0.878584298512076, 1.0872814156171304]]
     end
     @testset "similarity kernel test" begin
-        Random.seed!(111)
         @test similarity_kernel([1.0, 1.0], [1.1, 1.1], 1.0) == 0.9801986733067553
     end
     @testset "sparse linear explanations test" begin
-        Random.seed!(111)
         @test sparse_linear_explanations(x -> sum(x), [1.0, 1.0], [1.1, 1.1], 10, 1)[1] == 0.9706216694190505
     end
 end
