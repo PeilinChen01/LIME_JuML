2284abce9b369bbb85bd24a35917ce4296d74270
PeilinChen01 <708915207@qq.com>
2024-07-15 02:14:56 +0200

    Update LIME_JuML.jl

--- src/LIME_JuML.jl
+++ src/LIME_JuML.jl
@@ -4,6 +4,8 @@ module LIME_JuML
 include("lime.jl")
 include("superpixel_lime.jl")
 include("image_lime.jl")
+include("lenet5_lime1.jl")
+include("lenet5_lime2.jl")
 
 export LIME
 export predict_fn
@@ -15,5 +17,7 @@ export create_deactivation_matrix
 export perturb_image
 export plot_labels
 export explain_image
+export sample_around_image
+
 
 end
